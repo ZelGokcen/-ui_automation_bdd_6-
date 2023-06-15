de8d65021ecbf7d84021b9ad80e32dea6579203f
ZelGokcen <zelihaarmas@gmail.com>
2023-06-15 11:10:48 -0500

    Current Updates

---
+++ src/test/java/utils/Driver.java
@@ -0,0 +1,60 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+
+    private static WebDriver driver;
+
+    private Driver() {
+    } // constructor
+
+    public static WebDriver getDriver() { // method that set up the driver and returns
+        if (driver == null) { // this is when we do this >> if driver is null
+
+            //  WebDriverManager.chromedriver().setup(); // Set up Chrome driver
+
+            String browser = ConfigurationReader.getProperty("browser");
+
+            switch (browser){
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup(); // Set up driver
+                    driver = new FirefoxDriver(); // Launch a driver
+                    break;
+                case "safari":
+                    WebDriverManager.getInstance(SafariDriver.class);
+                    driver = new SafariDriver();
+                    break;
+                case "edge":
+                    // TODO implement Microsoft Edge driver instantiation
+                    break;
+                default:
+                    throw new IllegalStateException(browser + " browser does not match any case!!!");
+            }
+
+            driver.manage().window().maximize(); // Maximizes the window
+            driver.manage().timeouts().implicitlyWait(Integer.parseInt(ConfigurationReader.getProperty("implicit_wait")), TimeUnit.SECONDS); // implicit wait
+        }
+        return driver;
+    }
+
+
+    public static void quitDriver() {
+        if (driver != null) { // if not null, return existing object and quit the driver.
+            driver.manage().deleteAllCookies(); // to not get issues for next execution.
+            driver.quit();
+            driver = null; // make sure driver is null(nothing)
+        }
+
+    }
+}
---
+++ src/test/resources/features/Google.feature
@@ -0,0 +1,20 @@
+
+// Every feature MUST start with a Feature and one Feature only, but multiple Scenarios
+Feature: Google search functionality
+
+// Every test MUST start with a Scenario or Scenario Outline
+  //Scenario should have meaningful name.
+
+  @Google
+  Scenario Outline: Validate the title and url after search
+    Given user navigates to "https://www.google.com"
+    When user search for "<input>"
+    Then user should see "<input>" in the url
+    And user should see "<input>" in the title
+
+    Examples:
+      | input     |
+      | Tesla     |
+#      | Apple     |
+#      | Microsoft |
+#      | Amazon    |
